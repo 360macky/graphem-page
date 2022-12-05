23b104bb9041ef90e33c1ec6f026675502151d5b
Marcelo Arias <hello@marceloarias.com>
2022-12-05 08:45:30 -0500

    Add relevant links to the description

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,11 @@ function App() {
     <div className="flex flex-col items-center w-full dark:bg-[#1b1b1b]">
       <div className="flex flex-col w-full pt-[1rem] sm:pt-[4.5rem]">
         <div className="flex w-full p-[1rem] justify-center">
-          <img src={logotype} className="max-h-[6.8rem] graphem-particles" alt="Graphem logo" />
+          <img
+            src={logotype}
+            className="max-h-[6.8rem] graphem-particles"
+            alt="Graphem logo"
+          />
         </div>
         <div className="flex w-full justify-center">
           <img
@@ -32,16 +36,41 @@ function App() {
       <div className="flex justify-center pt-[1.5rem]">
         <div className="sm:w-[30rem]">
           <p className="px-[2rem] text-[1.1rem] text-center dark:text-white">
-            <b>Graphem</b> is an Open MCT plugin that allows you to connect a
-            telemetry source from GraphQL in real time. Through a data
-            dictionary, Graphem receives the historical data and continues the
-            connection through GraphQL-WS plotting the data in real time.
+            <b>Graphem</b> is an{" "}
+            <a
+              href="https://nasa.github.io/openmct/"
+              target={"_blank"}
+              rel="noreferrer"
+              className="text-[#5079F7]"
+            >
+              Open MCT plugin
+            </a>{" "}
+            that allows you to connect a telemetry source from{" "}
+            <a
+              href="https://graphql.org/"
+              target={"_blank"}
+              rel="noreferrer"
+              className="text-[#5079F7]"
+            >
+              GraphQL
+            </a>{" "}
+            in real time. Through a data dictionary, Graphem receives the
+            historical data and continues the connection through{" "}
+            <a
+              href="https://www.npmjs.com/package/graphql-ws"
+              target={"_blank"}
+              rel="noreferrer"
+              className="text-[#5079F7]"
+            >
+              GraphQL-WS
+            </a>{" "}
+            plotting the data in real time.
           </p>
         </div>
       </div>
-      <div className="flex justify-center pt-[2rem] gap-x-[1rem]">
+      <div className="flex justify-center pt-[2rem] flex-col sm:flex-row gap-y-[1rem] gap-x-[1rem]">
         <div
-          className="flex transition dark:border-[white] dark:bg-[#424242] bg-gradient-to-r from-[#5079F7] to-[#E535AB] hover:ring rounded-full py-[0.5rem] items-center px-[2rem] cursor-pointer active:scale-105"
+          className="flex justify-center transition dark:border-[white] dark:bg-[#424242] bg-gradient-to-r from-[#5079F7] to-[#E535AB] hover:ring rounded-full py-[0.5rem] items-center px-[2rem] cursor-pointer active:scale-105"
           onClick={copyCommand}
           tabIndex={0}
           title="Copy to clipboard"
@@ -59,11 +88,16 @@ function App() {
             alt="Copy to clipboard"
           />
         </div>
-        <a href="https://github.com/360macky/graphem/" target={"_blank"} rel="noreferrer">
+        <a
+          href="https://github.com/360macky/graphem/"
+          target={"_blank"}
+          rel="noreferrer"
+        >
           <div
             className="flex transition dark:border-[white] dark:bg-[#424242] bg-[#424242] hover:ring rounded-full py-[0.5rem] items-center px-[2rem] cursor-pointer active:scale-105"
             tabIndex={0}
             title="GitHub Repository"
+            role={"button"}
           >
             <p
               className="text-white text-[1.2rem] font-mono font-light"
@@ -74,7 +108,7 @@ function App() {
             <img
               src={link}
               className="h-[1.6rem] pl-[0.8rem]"
-              alt="Copy to clipboard"
+              alt="Go to GitHub Repository"
             />
           </div>
         </a>
@@ -107,6 +141,7 @@ function App() {
             href="https://github.com/360macky/graphem/blob/main/LICENSE"
             target={"_blank"}
             rel="noreferrer"
+            className="text-[#5079F7]"
           >
             LICENSE
           </a>{" "}
